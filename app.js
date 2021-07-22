1de2cd59eea28ed396a10b3ffaafd713c5a7c697
Ayushmaan Verma <56289697+thatssoayushmaan@users.noreply.github.com>
2021-07-22 17:59:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
 const express = require('express')
-const fs = require('fs')
+const serveIndex = require('serve-index')
 
-const Port = process.env.Port || 3000
-var result = ""
 const app = express()
 
-fs.readdir('./public', (err, files) => {
-    app.get('/', (req,res) => {
-        if(err){
-            console.log(err)
-        }else{
-            for(i=0;i<files.length;i++){
-                var path = './public/' + files[i]
-                var temp = fs.statSync(path)
-                if(temp.isFile()){
-                    result += '<img src="https://img.icons8.com/ios/50/000000/jp2.png"/>' + files[i] + '<br/>'
-                }  else{
-                    result += '<img src="https://img.icons8.com/ios/50/000000/pictures-folder.png"/>' + files[i] + '<br/>'
-                }
-            }
-            res.send(result)
-        }
-    })
-})
+app.use(
+  '/files',
+  express.static('public'),
+  serveIndex('public', { icons: true })
+)
 
-app.listen(5000, () => {
-    console.log('server up and running')
-})
+app.listen(3000, () => console.log('Server Up and Running'))
